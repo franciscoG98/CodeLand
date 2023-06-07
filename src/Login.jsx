38e3f8c59493431fa1dc9a75dc45cd9ae11d9fec
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-06-06 23:05:28 -0300

    clean up project defaults

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import './Login.css'
+
+function Login() {
+
+  return(
+    <>
+      <h1>Bienvenido a CodeLand!</h1>
+    </>
+  )
+
+}
+
+export default Login
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Login from './Login.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
